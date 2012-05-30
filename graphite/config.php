594354d75f26748b9ba0bb58ea96a552fb3c355b
Joel Bradbury <joelbradbury@gmail.com>
2012-05-31 00:52:57 +0200

    Updated to 2.0 with a completely refactored inclusion method

---
+++ graphite/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Graphite Config File *
+ * @package         graphite_ee_addon
+ * @version         2.0
+ * @author          Joel Bradbury ~ <joel@squarebit.co.uk>
+ * @link            http://squarebit.co.uk/graphite
+ * @copyright       Copyright (c) 2012, Joel 
+ */
+
+if ( ! defined('GRAPHITE_NAME'))
+{
+	define('GRAPHITE_NAME',         'Graphite');
+	define('GRAPHITE_CLASS_NAME',   'Graphite');
+	define('GRAPHITE_VERSION',      '2.0');
+	define('GRAPHITE_DOCS',         'http://squarebit.co.uk/graphite');
+
+}
+
+$config['name']    = GRAPHITE_NAME;
+$config['version'] = GRAPHITE_VERSION;
